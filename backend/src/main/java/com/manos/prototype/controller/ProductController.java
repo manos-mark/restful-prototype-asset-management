0f20a7eda93eb0c3df19255741f423e8e5010e6b
manos <manos-mark@hotmail.com>
2019-03-23 12:17:49 +0200

    products count get mapping

--- backend/src/main/java/com/manos/prototype/controller/ProductController.java
+++ backend/src/main/java/com/manos/prototype/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -74,9 +75,9 @@ public class ProductController {
 		return pageResultDto;
 	}
 
-	@PostMapping("/count")
-	public Long getProductsCountByStatus(@RequestBody StatusRequestDto statusId) {
-		return productService.getProductsCountByStatus(statusId.getStatusId());
+	@GetMapping("/count")
+	public Long getProductsCountByStatus(@RequestParam int statusId) {
+		return productService.getProductsCountByStatus(statusId);
 	}
 	
 	@GetMapping("/{id}")
