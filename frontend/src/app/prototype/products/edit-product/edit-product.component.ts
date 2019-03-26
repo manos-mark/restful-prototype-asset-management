57b18b6268e3599c74ff057d9bbd1d7e85d5fb7e
manos <manos-mark@hotmail.com>
2019-03-26 16:10:27 +0200

    upload unique picture

--- frontend/src/app/prototype/products/edit-product/edit-product.component.ts
+++ frontend/src/app/prototype/products/edit-product/edit-product.component.ts
@@ -167,19 +167,28 @@ export class EditProductComponent implements OnDestroy {
 
     onUploadPicture(event, picInput): void {
         const eventFileList = event.target.files;
-        this.pictures.push(new ProductPicture({
-            id: undefined,
-            productId: undefined,
-            name: eventFileList.item(0).name,
-            size: eventFileList.item(0).size,
-            file: eventFileList.item(0)
-        }))
-        this.computedPicturesLength++;
-        this.computedPicturesListSize += eventFileList.item(0).size;
-        this.thumbArray.push(new FormControl(null));
-        picInput.value = null;
-        if (!this.isThumbSelected) {
-            this.onSelectThumb(0);
+        let unique = true;
+        this.pictures.forEach(pic => {
+            if ( pic.name.indexOf(eventFileList.item(0).name) !== -1 ) {
+                unique = false;
+                return;
+            }
+        });
+        if (unique) {
+            this.pictures.push(new ProductPicture({
+                id: undefined,
+                productId: undefined,
+                name: eventFileList.item(0).name,
+                size: eventFileList.item(0).size,
+                file: eventFileList.item(0)
+            }));
+            this.computedPicturesLength++;
+            this.computedPicturesListSize += eventFileList.item(0).size;
+            this.thumbArray.push(new FormControl(null));
+            picInput.value = null;
+            if (!this.isThumbSelected) {
+                this.onSelectThumb(0);
+            }
         }
     }
 
